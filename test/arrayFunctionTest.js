542784ce72d245026e09fe62baf73ebd82180919
Pannapur <pannapur@in-Pannapur.local>
2018-11-21 10:02:26 +0530

    Migrate test cases to mocha framework and deleted swp files.

--- test/arrayFunctionTest.js
+++ test/arrayFunctionTest.js
@@ -1,12 +1,6 @@
 const assert = require("assert");
 const arrayFunctions = require("../src/arrayFunctions.js");
-const printLog = require("../testScript/logReporter.js").printLog;
 const {map,filter} = arrayFunctions;
-
-const checkAssert = function(functionName,args,actual,expected){
-  printLog(functionName,args,actual,expected);
-  assert.deepEqual(actual,expected);
-}
 
 const increment = function(number){
   return number + 1;
@@ -16,19 +10,37 @@ const checkAge = function(age){
   return age > 10;
 }
 
-const testMap = function(){
-  checkAssert("No values",[],map([],increment),[]);
-  checkAssert("Single value","2",map([0],increment),[1]);
-  checkAssert("Multiple values","2",map([1,2],increment),[2,3]);
-}
+describe('map' , function() {
+  describe('increment', function() {
+  it('check for null input', function() {
+    assert.deepEqual(map([],increment),[]);
+  })
+  it('check for single value', function() {
+    assert.deepEqual(map([0],increment),[1]);
+  })
+  it('check for multiple values', function() {
+    assert.deepEqual(map([1,2],increment),[2,3]);
+  })
+  })
+})
 
-const testFilter = function(){
-  checkAssert("No value",[],filter([],checkAge),[]);
-  checkAssert("lesser value",[1],filter([1],checkAge),[]);
-  checkAssert("greater value",[20],filter([20],checkAge),[20]);
-  checkAssert("multiple values",[20,30,40],filter([20,30,40],checkAge),[20,30,40]);
-  checkAssert("Mixed values",[1,2,30],filter([1,2,30],checkAge),[30]);
-}
+describe('filter', function() {  
+  describe('checkAge', function() {
+    it('check for null input', function() {
+      assert.deepEqual(filter([],checkAge),[]);
+    })
+    it('check for lesser value', function() {
+      assert.deepEqual(filter([1],checkAge),[]);
+    })
+    it('check with single greater value', function() {
+      assert.deepEqual(filter([20],checkAge),[20]);
+    })
+    it('check with multiple greater values', function() {
+      assert.deepEqual(filter([20,30,40],checkAge),[20,30,40]);
+    })
+    it('check with combination of values', function() {
+      assert.deepEqual(filter([1,2,30],checkAge),[30]);
+    })
+  })
+})
 
-testFilter();
-testMap();
